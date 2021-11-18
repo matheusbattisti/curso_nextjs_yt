94046837db3266854410850374ffe2de854ba192
Matheus Battisti <matheusbudkewicz@gmail.com>
2021-11-18 09:57:05 -0200

    paginas e rotas

---
+++ 1_hello_world_next/pages/index.js
@@ -0,0 +1,7 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return <h1>Hello World Next!</h1>
+}
