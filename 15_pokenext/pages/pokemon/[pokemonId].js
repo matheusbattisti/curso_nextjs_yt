2c233309342b062a8eb271fc91c6eb499e4f5ba6
Matheus Battisti <matheusbudkewicz@gmail.com>
2021-11-23 15:28:47 -0300

    continuando projeto

---
+++ 15_pokenext/pages/pokemon/[pokemonId].js
@@ -0,0 +1,75 @@
+import styles from "../../styles/Pokemon.module.css";
+
+import Image from "next/image";
+
+export const getStaticPaths = async () => {
+  const maxPokemons = 251;
+  const api = `https://pokeapi.co/api/v2/pokemon/`;
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+
+  const data = await res.json();
+
+  const paths = data.results.map((pokemon, index) => {
+    return {
+      params: { pokemonId: index.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.pokemonId;
+
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+  const data = await res.json();
+
+  return {
+    props: { pokemon: data },
+  };
+};
+
+export default function Pokemon({ pokemon }) {
+  console.log(pokemon);
+  return (
+    <div className={styles.pokemon_container}>
+      <h1>{pokemon.name}</h1>
+      <Image
+        src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`}
+        width="120"
+        height="120"
+        alt={pokemon.name}
+      />
+      <div>
+        <h3>Número:</h3>
+        <p>#{pokemon.id}</p>
+      </div>
+      <div>
+        <h3>Tipos:</h3>
+        <div className={styles.types_container}>
+          {pokemon.types.map((item, index) => (
+            <span
+              key={index}
+              className={`${styles.type} ${styles["type_" + item.type.name]}`}
+            >
+              {item.type.name}
+            </span>
+          ))}
+        </div>
+      </div>
+      <div>
+        <h3>Altura:</h3>
+        <p>{pokemon.height * 10} cm</p>
+      </div>
+      <div>
+        <h3>Peso:</h3>
+        <p>{pokemon.weight / 10} kg</p>
+      </div>
+    </div>
+  );
+}
